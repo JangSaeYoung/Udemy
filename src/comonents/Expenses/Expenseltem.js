f4ad2b8f6d456d44d6f2d890cbed9d39944d5ec5
saeyoung <syoungbb25@gmail.com>
2022-03-05 19:20:44 +0900

    섹션4. useState

--- src/comonents/Expenses/Expenseltem.js
+++ src/comonents/Expenses/Expenseltem.js
@@ -1,25 +1,37 @@
 // 비용 아이템을 만드는 컴포넌트
-// 파일 이름을 보고 어떤 로직과 HTML코드가 파일 안에 있을지 알수 있다. 
-// => 즉, 파일명은 로직과 HTML코드를 예상할 수 있는 이름으로 지어야한다. 
 
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => { // (1) props 가져올거야
+     // function clickHandler() {}
+    const [title, setTitle] = useState(props.title);  // dafault 값. 컴포넌트 함수 내에서만 작동한다. 
+
+    const clickHandler = () => {
+       setTitle('Updated!!!'); // 
+        console.log(title);
+    } 
 
     return (
     <Card className="expense-item">
         <ExpenseDate date={props.date}/>
         <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
         </div>
+        <button onClick = {clickHandler}>Change Title</button> 
+        {/* 버튼을 클릭 시 title 출력이 달라진다.  */}
     </Card>
 );
 }
 
 export default ExpenseItem;
 
-
-
+//State 
+//왜 const를 사용하는 가?
+// 컴포넌트 인스턴스 기반 단위 => 즉, 하나 이상의 컴포넌트를 생성할 때도 개별 상태를 가지게 된다. 
+// 상태는 컴포넌트 인스턴스 기반 단위로 나누어져 있다!!
+// 새로운 값을 할당 할 거면서 왜 상수를 사용했을까? => '='를 사용하여 값을 할당하지 않았다. 
+// useState를 불러옴으러써 React에게 우리를 위해 값을 manage해야 한다고 말하는 거다. 
